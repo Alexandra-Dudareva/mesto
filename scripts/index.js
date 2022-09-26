a65f9c1ef7e635a1a66bcc3bf40e306ef3954a06
Alexandra-Dudareva <peppasvin7@yandex.ru>
2022-09-26 05:51:16 +0300

    Александра Дударева. Проектная работа 4, версия 1.

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const popupOpenButton = document.querySelector('.profile__open-popup');
+const popup = document.querySelector('.popup');
+const popupCloseButton = popup.querySelector('.popup__close');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
+const profileForm = popup.querySelector('.form');
+const inputName = profileForm.querySelector('.form__name');
+const inputStatus = profileForm.querySelector('.form__status');
+
+popupToggle = function () {
+    popup.classList.toggle('popup__opened');
+}
+
+formSubmitHandler = function (evt) {
+    evt.preventDefault();
+
+    profileName.textContent = inputName.value;
+    profileStatus.textContent = inputStatus.value;
+
+    popupToggle();
+}
+
+profileForm.addEventListener('submit', formSubmitHandler);
+
+popupOpenButton.addEventListener('click', popupToggle);
+
+popupCloseButton.addEventListener('click', popupToggle);
+
+
