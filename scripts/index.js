3a49e43439fb589a3f8038c48fdf3872f74ef970
Alexandra-Dudareva <peppasvin7@yandex.ru>
2022-10-14 04:29:09 +0300

    Александра Дударева. Проектная работа 5, версия 1.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const popupCloseButton = popup.querySelector('.popup__close');
+const popupCloseButton = popup.querySelector('.popup__close-button');
 const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
 const profileForm = popup.querySelector('.form');
@@ -9,6 +9,9 @@ const inputStatus = profileForm.querySelector('.form__input_type_status');
 
 /*ПР5. Кнопка лайк*/
 const likeButton = document.querySelector(".element__like-button");
+
+/*ПР5. Открытие картинки*/
+const OpenImageButton = document.querySelector(".element__image");
 
 /*ПР 4*/
 popupToggle = function () {
@@ -37,6 +40,10 @@ likeToggle = function (){
     likeButton.classList.toggle('element__like-button_active');
 }
 
+/*ПР5. Открытие картинки*/
+openImageToggle = function (){
+    OpenImageButton.classList.toggle('popup_opened');
+}
 
 /*ПР 4*/
 profileForm.addEventListener('submit', formSubmitHandler);
@@ -47,3 +54,6 @@ popupCloseButton.addEventListener('click', popupToggle);
 
 /*ПР5. Кнопка лайк*/
 likeButton.addEventListener('click', likeToggle);
+
+/*ПР5. Открытие картинки*/
+OpenImageButton.addEventListener('click', openImageToggle);
