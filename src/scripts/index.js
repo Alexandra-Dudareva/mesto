c68af40c6dbd9152158b3e54536461a491c1552a
Alexandra-Dudareva <peppasvin7@yandex.ru>
2023-06-04 01:36:17 +0300

    Александра Дударева. Проектная работа 8, версия 1.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,11 @@
-import Card from "../components/Сard.js";
-import FormValidator from "../components/FormValidator.js";
-import initialCards from "../utils/initialCards.js";
-import Section from "../components/Section.js";
-import PopupWithImage from "../components/PopupWithImage.js";
-import PopupWithForm from "../components/PopupWithForm.js";
-import UserInfo from "../components/UserInfo.js";
+import Card from "../../components/Сard.js";
+import FormValidator from "../../components/FormValidator.js";
+import initialCards from "../../utils/initialCards.js";
+import Section from "../../components/Section.js";
+import PopupWithImage from "../../components/PopupWithImage.js";
+import PopupWithForm from "../../components/PopupWithForm.js";
+import UserInfo from "../../components/UserInfo.js";
+import '../pages/index.css';
 
 const popupOpenButtonEdit = document.querySelector('.profile__edit-button');
 const formEditProfile = document.querySelector('#formEditProfile');
