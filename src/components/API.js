d845bb69cfcffe1ac4faf5d25d52d7bd9e583178
Alexandra-Dudareva <peppasvin7@yandex.ru>
2023-06-15 16:47:54 +0300

    Александра Дударева. Проектная работа 9, версия 2.

---
+++ src/components/API.js
@@ -0,0 +1,101 @@
+export class API {
+    constructor(url, authorization) {
+        this._URL = url;
+        this._authorization = authorization;
+    }
+
+    _checkResult(res){
+        if (res.ok) {
+            return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+    }
+
+
+    getProfile(){
+        return fetch(`${this._URL}/users/me`, {
+            headers: {
+                authorization: this._authorization
+            }
+        }).then(this._checkResult)
+    }
+
+    getInitialCards() {
+        return fetch(`${this._URL}/cards`, {
+            headers: {
+                authorization: this._authorization
+            }
+        }).then(this._checkResult)
+    }
+
+    postCard(item) {
+        return fetch(`${this._URL}/cards`, {
+            method: `POST`,
+            headers: {
+                authorization: this._authorization,
+                'Content-Type': `application/json`
+            },
+            body: JSON.stringify({
+                name: item.place,
+                link: item.link
+            })
+        }).then(this._checkResult)
+    }
+
+    patchProfile(item) {
+        return fetch(`${this._URL}/users/me`, {
+            method: `PATCH`,
+            headers: {
+                authorization: this._authorization,
+                'Content-Type': `application/json`
+            },
+            body: JSON.stringify({
+                name: item.name,
+                about: item.about
+            })
+        }).then(this._checkResult)
+    }
+
+    patchAvatar(item) {
+        return fetch(`${this._URL}/users/me/avatar`, {
+            method: `PATCH`,
+            headers: {
+                authorization: this._authorization,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                avatar: item.avatar
+            })
+        }).then(this._checkResult)
+    }
+
+    putLike(id) {
+        return fetch(`${this._URL}/cards/${id}/likes`,
+            {
+                method: `PUT`,
+                headers: {
+                    authorization: this._authorization,
+                }
+            }).then(this._checkResult)
+    }
+
+    deleteLike(id) {
+        return fetch(`${this._URL}/cards/${id}/likes`,
+            {
+                method: `DELETE`,
+                headers: {
+                    authorization: this._authorization,
+                }
+            }).then(this._checkResult)
+    }
+
+    deleteCard(id) {
+        return fetch(`${this._URL}/cards/${id}`,
+            {
+                method: `DELETE`,
+                headers: {
+                    authorization: this._authorization,
+                }
+            }).then(this._checkResult)
+    }
+}
